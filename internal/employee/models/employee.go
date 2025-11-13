a58bfd508998da0ad5e274998b819edef7b96b5f
Nikolay Parshakov <parshakov@retailcrm.ru>
2025-11-13 13:10:14 +0300

    Improve employee model

--- internal/employee/models/employee.go
+++ internal/employee/models/employee.go
@@ -1,9 +1,10 @@
 package models
 
 type Employee struct {
-	Id     int    `json:"id" validate:"required"`
-	Name   string `json:"name" validate:"required"`
-	Sex    string `json:"sex" validate:"required"`
-	Age    int    `json:"age" validate:"required"`
-	Salary int    `json:"salary" validate:"required"`
+	Id       int    `json:"id" validate:"required"`
+	Name     string `json:"name" validate:"required,max=255"`
+	Sex      string `json:"sex" validate:"required,oneof=male female"`
+	Age      int    `json:"age" validate:"required"`
+	Position string `json:"position" validate:"required,max=255"`
+	Salary   int    `json:"salary" validate:"required"`
 }
