a590b751b885b8359c71f60544803ee50f392f3e
Nikolay Parshakov <parshakov@retailcrm.ru>
2026-02-06 08:11:04 +0300

    Add bson tags for department model

--- internal/department/models/department.go
+++ internal/department/models/department.go
@@ -2,7 +2,7 @@ package models
 
 type Department struct {
 	Id          int    `json:"id" validate:"required"`
-	RootId      int    `json:"root_id"`
+	RootId      int    `json:"root_id" bson:"root_id"`
 	Name        string `json:"name" validate:"required,max=255"`
-	EmployeeIds []int  `json:"employee_ids"`
+	EmployeeIds []int  `json:"employee_ids" bson:"employee_ids"`
 }
